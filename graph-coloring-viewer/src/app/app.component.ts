a42aaa5052fca8f4795f5b33b4474124b9b475fb
Tiago <tiagogoncalves@discente.ufg.br>
2024-07-05 09:57:27 -0300

    + Update

--- graph-coloring-viewer/src/app/app.component.ts
+++ graph-coloring-viewer/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { Vertice } from './interfaces/vertice';
+import { Edge } from './interfaces/edge';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  
+  public vertices: Vertice[] = [];
+  public edges: Edge[] = [];
+
+  public setSeparatorCharacter = '\n\n';
+  public elementSeparatorCharacter = '\n';
 
   handleUpload(event: any) {
     const file: File = event.files[0];
@@ -19,10 +25,34 @@ export class AppComponent {
 
     reader.onload = () => {
       const fileContent: any = reader.result;
-      console.log('Conteúdo do arquivo:', fileContent);
+      this.formatData(fileContent);
     };
 
     reader.readAsText(file);
   }
-  
+
+  private formatData(text: string): void {
+    const textSplit: string[] = text.split(this.setSeparatorCharacter);
+
+    const verticeSet = textSplit[0].split(this.elementSeparatorCharacter);
+    const edgeSet = textSplit[1].split(this.elementSeparatorCharacter);
+
+    let newVertices: Vertice[] = []
+
+    verticeSet.forEach((vertice: string) => {
+      const element = vertice.slice(1, vertice.length - 1).split(',');
+      newVertices.push({index: parseInt(element[0]),color: parseInt(element[1])});
+    });
+
+    this.vertices = newVertices;
+
+    let newEdges: Edge[] = []
+
+    edgeSet.forEach((edge: string) => {
+      const element = edge.slice(1, edge.length - 1).split(',');
+      newEdges.push({aVerticeIndex: parseInt(element[0]), bVerticeIndex: parseInt(element[1]), color: parseInt(element[2])});
+    });
+
+    this.edges = newEdges;
+  }
 }
--- graph-coloring-viewer/src/app/components/graph-viewer/graph-viewer.component.ts
+++ graph-coloring-viewer/src/app/components/graph-viewer/graph-viewer.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from "@angular/core";
+import { AfterViewInit, Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from "@angular/core";
 import { Edge } from "src/app/interfaces/edge";
 import { Vertice } from "src/app/interfaces/vertice";
 
@@ -8,14 +8,17 @@ import { Vertice } from "src/app/interfaces/vertice";
     templateUrl: './graph-viewer.component.html',
     styleUrls: ['./graph-viewer.component.scss']
 })
-class GraphViewerComponent implements AfterViewInit {
+class GraphViewerComponent implements AfterViewInit, OnChanges {
+    ngOnChanges(changes: SimpleChanges): void {
+        if (this.canvas !== undefined) this.drawGraph();
+    }
     @Input() vertices: Vertice[] = [];
     @Input() edges: Edge[] = [];
 
     @ViewChild('myCanvas') canvas!: ElementRef<HTMLCanvasElement>;
 
     public ngAfterViewInit(): void {
-        this.drawGraph();
+        if (this.vertices.length !== 0) this.drawGraph();
     }
 
     public drawGraph() {
@@ -26,22 +29,47 @@ class GraphViewerComponent implements AfterViewInit {
         let centerX = this.canvas.nativeElement.width / 2;
         let centerY = this.canvas.nativeElement.height / 2;
 
-        let numPonints = 100;
+        let numPonints = this.vertices.length;
 
-        let radius = 300;
+        let radius = 100;
         const angleIncrement = (2 * Math.PI) / numPonints;
 
         ctx.fillStyle = 'grey';
+    
+        this.edges.forEach((edge: any) => {
+            let angle;
+            const v1 = this.vertices[edge[0]];
+            const v2 = this.vertices[edge[1]];
 
-        for (let i = 0; i < numPonints; i++) {
-            const angle = i * angleIncrement;
+                angle
+            const startX = centerX + radius * Math.cos(angle);
+            const startY = centerY + radius * Math.sin(angle);
+
+            var startX = v1.index;
+            var startY = v1.index;
+            var endX = v2[0];
+            var endY = v2[1];
+
+            ctx.beginPath();
+            ctx.moveTo(startX, startY); // Move para o ponto inicial
+            ctx.lineTo(endX, endY);     // Desenha uma linha até o ponto final
+            ctx.strokeStyle = 'black'; // Define a cor da linha (vermelho)
+            ctx.lineWidth = 2;          // Define a largura da linha
+            ctx.stroke();               // Renderiza a linha
+
+            // Fecha o caminho (path)
+            ctx.closePath();
+        });
+
+        this.vertices.forEach((vertice: Vertice) => {
+            const angle = vertice.index * angleIncrement;
             const x = centerX + radius * Math.cos(angle);
             const y = centerY + radius * Math.sin(angle);
 
             ctx.beginPath();
-            ctx.arc(x, y, 5, 0, 2 * Math.PI);
+            ctx.arc(x, y, 10, 0, 2 * Math.PI);
             ctx.fill();
-        }
+        });
     }
 }
 
