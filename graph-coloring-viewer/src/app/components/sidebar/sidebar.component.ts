a26449e9fb083763bd33a92a3a3665497faa2fc7
James <tiagogoncalves@discente.ufg.br>
2024-08-02 12:31:20 -0300

    + Update

---
+++ graph-coloring-viewer/src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    standalone: true,
+    selector: 'app-sidebar',
+    templateUrl: './sidebar.component.html',
+    styleUrls: ['./sidebar.component.scss'],
+})
+class SidebarComponent {
+
+}
